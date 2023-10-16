b37478624f2151ed6333bada719ce7e7d8e08915
sejin <sejin.choe@gmail.com>
2023-10-16 18:32:26 +0900

    tf v3 - classification

--- tensorflow/06_tf_classification.py
+++ tensorflow/06_tf_classification.py
@@ -1,31 +1,142 @@
-"""## DNN(MLP) 모델을 이용한 MNIST 데이터 셋 분류
-* 체점 기준 :
-  - 데이터 셋 : 체점 서버내 테스트 데이터 셋
-  - 성능 지표 : Accuracy
-  - PASS 기준 : 80% 이상
-"""
-
 import tensorflow as tf
 from keras.datasets import fashion_mnist
+from keras import models, layers
+
+import numpy as np
 import matplotlib.pyplot as plt
 
-"""* Step 1. Inptu tensor 와 Target tensor 준비(훈련데이터)"""
-
-# label 데이터의 각 value 에 해당하는 class name 정보
+# 데이터셋 임포트, 데이터 탐색
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-# 수강생 작성 코드
-# 1. import 한 fashion_mnist API를 이용하여 fashion_mnist 데이터 셋을 다운로드 받으세요
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+train_images.shape
+train_labels.shape
+test_images.shape
+test_labels.shape
+
+# 데이터의 전처리
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+#train_images = train_images.reshape(60000, 28*28) / 255.0
+#test_images = test_images.reshape(10000, 28*28) / 255.0
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
 
 
 
-"""* Step 2. 데이터의 전처리 """
+# 모델 구성
+model = models.Sequential([
+    layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10)
+])
 
-# 수강생 작성 코드
-# 1. 3차원 형태(batch, hight, width)의 train, test feature 데이터를 2차원(batch, hight*width)으로 변경 하세요
-# 2. feature 데이터를 [0, 1] 사이로 scailing을 수행하세요
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
+# 모델 훈련
+model.fit(train_images, train_labels, epochs=10)
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+predictions = probability_model.predict(test_images)
+predictions[0]
+np.argmax(predictions[0])
+test_labels[0]
+
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                    100*np.max(predictions_array),
+                                    class_names[true_label]),
+                                    color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels)
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions[i], test_labels)
+plt.tight_layout()
+plt.show()
+
+# Grab an image from the test dataset.
+img = test_images[1]
+img.shape
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+img.shape
+
+predictions_single = probability_model.predict(img)
+predictions_single
+
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+np.argmax(predictions_single[0])
 
 # 수강생 작성 코드
 # 1. 1차원 형태의(batch, ) class index 인 train, test label 데이터를
